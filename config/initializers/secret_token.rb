be361b96e6c27b86f867144d40860e4953c6547b
rangerrick13 <rangerrick13@yahoo.com>
2013-02-16 14:25:10 -0500

    Initial Creation of Suggestotron app. Votes model action.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = 'e6e62a6dec0ac7cb88b318edf39a23fa0f7b27504181ba98712cd58391795b2a80f15ee7418802fbe2e02527379a793625a2585a72442bc6c02a19f2249275a1'
